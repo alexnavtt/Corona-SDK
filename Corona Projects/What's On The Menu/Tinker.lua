8d80c7fc2249a25c5e9297587812d7b803c30608
alexnavtt <alex.j.navarro@outlook.com>
2020-07-13 12:28:17 -0500

    Added selection to text field, needs optimization

--- Corona Projects/What's On The Menu/Tinker.lua
+++ Corona Projects/What's On The Menu/Tinker.lua
@@ -1,19 +1,20 @@
 tinker = {}
 
 function tinker.newTextField(x, y, width, height, params)
-	-- params: rounded, backgroundColor, strokeColor, strokeWidth, cursorColor, textColor, defaultText, font
+	-- params: rounded, backgroundColor, strokeColor, strokeWidth, cursorColor, textColor, defaultText, font, selectColor
 	params = params or {}
 
 	-- Set default values
-	local rounded = params.rounded
-	local backgroundColor = params.backgroundColor or {0.5}
-	local strokeColor = params.strokeColor or {0}
-	local strokeWidth = params.strokeWidth or {0}
-	local cursorColor = params.cursorColor or {1}
-	local textColor = params.textColor or {1}
-	local font = params.font or native.systemFont
-	local defaultText = params.defaultText or "TEXT HERE"
-	local id = params.id or "Tinker_Text_Field"
+	local rounded 			= params.rounded
+	local backgroundColor 	= params.backgroundColor 	or {1}
+	local strokeColor 		= params.strokeColor 		or {0}
+	local strokeWidth 		= params.strokeWidth 		or {0}
+	local cursorColor 		= params.cursorColor 		or {1}
+	local textColor 		= params.textColor 			or {1}
+	local selectColor 		= params.selectColor 		or {0}
+	local font 				= params.font 				or native.systemFont
+	local defaultText 		= params.defaultText 		or "TEXT HERE"
+	local id 				= params.id 				or "Tinker_Text_Field"
 
 	local NTF = native.newTextField(-display.contentWidth, -display.contentHeight, 500, 100)  -- Native Text Field: Out of sight, out of mind
 	local TTF = display.newGroup()  -- Tinker Text Field
@@ -25,7 +26,7 @@ function tinker.newTextField(x, y, width, height, params)
 
 	-- Determine if the Text Field should have corners or rounded edges
 	local radius = 0
-	if rounded then
+	if rounded == true then
 		radius = height/2
 	end
 
@@ -54,8 +55,8 @@ function tinker.newTextField(x, y, width, height, params)
 	local function timerFunc(event)
 		cursor.alpha = 1 - cursor.alpha
 	end
-	local timerHandle = timer.performWithDelay(500, timerFunc, -1)
-	timer.pause(timerHandle)
+	NTF.timerHandle = timer.performWithDelay(500, timerFunc, -1)
+	timer.pause(NTF.timerHandle)
 
 	local text = display.newText({text = defaultText, 
 								  x = line_x,
@@ -102,8 +103,7 @@ function tinker.newTextField(x, y, width, height, params)
 			tried_width = 0.95*ghostText.width
 
 			if tried_width >= selection_width then
-				print("Succeeded at tried_width = " .. tried_width)
-				letter_index = math.max(i-1,1)
+				letter_index = math.max(i-1,0)
 				ghostText.text = original_text
 				return letter_index, offset
 			end
@@ -124,7 +124,7 @@ function tinker.newTextField(x, y, width, height, params)
 	-- Update TTF Text and Cursor Position
 	local function textFieldUpdateListener(event)
 		if event.phase == "began" then
-			timer.resume(timerHandle)
+			timer.resume(NTF.timerHandle)
 
 			if NTF.text == "" then
 				NTF.fresh = true
@@ -153,25 +153,72 @@ function tinker.newTextField(x, y, width, height, params)
 					cursor.x = cursor.x + text.width - old_width
 				end
 			end
+
+			if bkgd.display_box then
+				cursor.x = line_x + math.min(bkgd._offset_end, bkgd._offset_start)
+				bkgd.display_box:removeSelf()
+				bkgd.display_box = nil
+			end
 		end
 
 		if event.phase == "ended" or event.phase == "submitted" then
-			timer.pause(timerHandle)
+			timer.pause(NTF.timerHandle)
 			cursor.alpha = 0
 			native.setKeyboardFocus(nil)
+
+			if bkgd.display_box then
+				bkgd.display_box:removeSelf()
+				bkgd.display_box = nil
+			end
 		end
 	end
 	NTF:addEventListener("userInput", textFieldUpdateListener)
 
 	-- Decide where to put the cursor, and 
 	function bkgd:tap(event)
+		native.setKeyboardFocus(nil)
 		native.setKeyboardFocus(NTF)
-		local start_x = bkgd:localToContent(line_x, 0)
+		local start_x = self:localToContent(line_x, 0)
 		local letter_index, offset = findCursorLocation(event.x - start_x)
 		cursor.x = line_x + offset
 		NTF:setSelection(letter_index,letter_index)
+		return true
 	end
 	bkgd:addEventListener("tap", bkgd)
+
+	-- Add text selection 
+	function bkgd:touch(event)
+		if event.phase == "began" then
+			self._start_letter_index, self._offset_start = findCursorLocation(event.x - self:localToContent(line_x, 0))
+			display.getCurrentStage():setFocus(self)
+
+			local function timerFunc(event)
+				self._is_selecting = true
+				self.display_box = display.newRect(TTF, line_x + self._offset_start, text.y - 0.5*text.height, 1/1000*width, text.height)
+				self.display_box:setFillColor(unpack(selectColor), 0.3)
+				self.display_box.anchorX = 0
+				print("Selection started")
+			end
+			self._timerHandle = timer.performWithDelay(700, timerFunc, 1)
+
+		elseif event.phase == "moved" then
+			timer.cancel(self._timerHandle)
+			if self._is_selecting then
+				self._end_letter_index, self._offset_end = findCursorLocation(event.x - self:localToContent(line_x,0 ))
+				self.display_box.path.x3 = self._offset_end - self._offset_start
+				self.display_box.path.x4 = self.display_box.path.x3
+				NTF:setSelection(math.min(self._start_letter_index, self._end_letter_index), math.max(self._start_letter_index, self._end_letter_index))
+			end
+
+		elseif event.phase == "ended" or event.phase == "cancelled" then
+			timer.cancel(self._timerHandle)
+			display.getCurrentStage():setFocus(nil)
+			self._is_selecting = false
+
+		end
+
+	end
+	bkgd:addEventListener("touch", bkgd)
 
 
 	-- ############# --
@@ -187,6 +234,15 @@ function tinker.newTextField(x, y, width, height, params)
 		NTF.text = new_text
 		text.text = new_text
 		ghostText.text = new_text
+	end
+
+	function TTF:removeSelf()
+		timer.cancel(NTF.timerHandle)
+		NTF:removeSelf()
+		for i = 1,TTF.numChildren,1 do
+			TTF:remove(1)
+		end
+		TTF = nil
 	end
 
 	return TTF
